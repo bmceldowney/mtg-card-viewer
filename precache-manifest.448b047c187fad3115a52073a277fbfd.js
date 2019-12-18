6cd8444d0337c288fddb4d75adcf3c4f77664747
Ben McEldowney <ben.mceldowney@base2s.com>
2019-12-17 23:01:35 -0800

    Updates

---
+++ precache-manifest.448b047c187fad3115a52073a277fbfd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283c54ef649418449954799dcd76eb5e",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "902bd59cdd0058b8ab8b",
+    "url": "/myapp/static/css/2.bfd9c2b6.chunk.css"
+  },
+  {
+    "revision": "1ff35dfbd0546011a15f",
+    "url": "/myapp/static/css/main.5c728660.chunk.css"
+  },
+  {
+    "revision": "902bd59cdd0058b8ab8b",
+    "url": "/myapp/static/js/2.fd78bcf8.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/myapp/static/js/2.fd78bcf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ff35dfbd0546011a15f",
+    "url": "/myapp/static/js/main.4296d680.chunk.js"
+  },
+  {
+    "revision": "78bfe4f4f067ace97041",
+    "url": "/myapp/static/js/runtime-main.0f287211.js"
+  },
+  {
+    "revision": "3133a69e06f17ae34f71892cac501fb2",
+    "url": "/myapp/static/media/MPlantin-Bold.3133a69e.woff2"
+  },
+  {
+    "revision": "6588572655ae334c14bf586e68095748",
+    "url": "/myapp/static/media/MPlantin-Bold.65885726.woff"
+  },
+  {
+    "revision": "51be4de6bced10d880d1fa8141a365d2",
+    "url": "/myapp/static/media/MPlantin.51be4de6.woff"
+  },
+  {
+    "revision": "7e7a5be557f357f3ae17d6a72b70de4b",
+    "url": "/myapp/static/media/MPlantin.7e7a5be5.woff2"
+  },
+  {
+    "revision": "9e386489440385187900eaef333ab770",
+    "url": "/myapp/static/media/MagicMedieval.9e386489.woff"
+  },
+  {
+    "revision": "a3c809de96f7d6c6ec15e2d003535598",
+    "url": "/myapp/static/media/MagicMedieval.a3c809de.woff2"
+  },
+  {
+    "revision": "4c4de40c1f54c18a430a134994d10097",
+    "url": "/myapp/static/media/green_magic_background.4c4de40c.png"
+  },
+  {
+    "revision": "42bcdfd3a302cc5d46334cf44a855563",
+    "url": "/myapp/static/media/loading_spinner.42bcdfd3.png"
+  },
+  {
+    "revision": "4cce96920865afb5d9c7444870de9ae7",
+    "url": "/myapp/static/media/red_loading_spinner.4cce9692.png"
+  },
+  {
+    "revision": "61a5afdabf5a8988fdd071b5361ae4c2",
+    "url": "/myapp/static/media/red_magic_background.61a5afda.png"
+  },
+  {
+    "revision": "7249fe3541a7118df6e7a5456e3aa58f",
+    "url": "/myapp/static/media/white_magic_background.7249fe35.png"
+  }
+]);
